d1f23934a5e14a7d022081d43664455c927ef9fe
Andres <andresbolanosreyes@gmail.com>
2019-02-05 09:47:50 -0600

    added tests

---
+++ Tarea 5/Código/CatalogoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatalogoTest {
+
+    @Test
+    public void esNegativo() {
+
+    }
+
+    @Test
+    public void afueraDeLosLimites() {
+
+    }
+
+    @Test
+    public void verificarCorrimiento() {
+
+    }
+
+}
