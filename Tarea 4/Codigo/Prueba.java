de9cf9d822a01e14416ebba041c7a4820ba59246
andresryes <andresbolanosreyes@gmail.com>
2019-02-05 09:45:16 -0600

    update prueba

--- Tarea 4/Codigo/Prueba.java
+++ Tarea 4/Codigo/Prueba.java
@@ -3,27 +3,20 @@ import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 public class Prueba {
-	Main m = new Main();
 
    @Test
    public void esNegativo() {
-      System.out.println("Inside testSalutationMessage()");
-      message = "Hi!" + "Robert";
-      assertEquals(message,messageUtil.salutationMessage());
+
    }
    
    @Test
    public void afueraDeLosLimites() {
-      System.out.println("Inside testSalutationMessage()");
-      message = "Hi!" + "Robert";
-      assertEquals(message,messageUtil.salutationMessage());
+
    }
    
    @Test
    public void verificarCorrimiento() {
-      System.out.println("Inside testSalutationMessage()");
-      message = "Hi!" + "Robert";
-      assertEquals(message,messageUtil.salutationMessage());
+
    }
    
    
