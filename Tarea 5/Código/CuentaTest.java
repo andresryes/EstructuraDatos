147787c2512a1ff1a646629d42bee026fca8af01
Andres <andresbolanosreyes@gmail.com>
2019-02-05 09:50:17 -0600

    added tests

---
+++ Tarea 5/Código/CuentaTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+    @Test
+    public void esNegativo() {
+
+    }
+
+    @Test
+    public void afueraDeLosLimites() {
+
+    }
+
+    @Test
+    public void verificarCorrimiento() {
+
+    }
+}
