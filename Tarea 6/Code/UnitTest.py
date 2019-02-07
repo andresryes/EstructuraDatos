faa96749347fcba5a3a937104e83d80d6023736e
Andres <andresbolanosreyes@gmail.com>
2019-02-07 07:46:35 -0600

    added test

--- Tarea 6/Code/UnitTest.py
+++ Tarea 6/Code/UnitTest.py
@@ -1,4 +1,5 @@
 from Function import histogramFunction
+import unittest
 
 stringArray = list("aaaabbccc")
 
@@ -8,3 +9,7 @@ print(histogramTest)
 
 if(histogramTest[1][0] == 4 and histogramTest[1][1] == 3 and histogramTest[1][2] == 2):
     print("test worked perfectly")
+
+
+def test_upper(self):
+    self.assertEqual(histogramTest[2][0], 'a')
