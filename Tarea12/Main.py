6a1c4fc762f2c15af0cae2426c8e16dae33c3336
Andrés Bolaños <andr.9810@hotmail.com>
2019-04-30 08:44:29 -0600

    justificación[

--- Tarea12/Main.py
+++ Tarea12/Main.py
@@ -1,5 +1,10 @@
 from HashTable import *
+from memory_profiler import profile
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
 
+
+@profile
 def main():
     hash_table = HashTable()
 
@@ -9,6 +14,18 @@ def main():
     for x in hash_table.getHashTable():
         print(len(x))
         print (*x) 
+'''
+graphviz = GraphvizOutput(output_file='call_tree.png')
+with PyCallGraph(output=graphviz):
+    hash_table = HashTable()
+
+    for x in range(20):
+        hash_table.insert(x, str(x))
+
+    for x in hash_table.getHashTable():
+        print(len(x))
+        print (*x) 
+'''
 
 if __name__ == "__main__":
     main()
